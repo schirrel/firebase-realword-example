d5e018a309bae9d0fc02d918314e6c5574ac14e8
Alan Schio <6757777+schirrel@users.noreply.github.com>
2020-12-31 07:17:16 -0400

    Create products omponents with api and list

--- pages/products/list.js
+++ pages/products/list.js
@@ -1,18 +1,49 @@
+import { products } from '../../services/Api.js';
+
 export default Vue.component("products", {
   name: "Products",
   data: function () {
     return {
+      list: []
     };
   },
   created: function () {
+    this.init();
   },
   methods: {
     init() {
-    
+      products.list().then((snapshot) => {
+        this.list = snapshot
+      });
+    },
+    remove($event, key) {
+      products.delete(key);
+      this.init();
     }
 
   },
   template: `
-<h1> Hi you are at Products </h1>
+
+
+  <table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Position</th>
+      <th width="80px">
+      <router-link class="nav-link" :to="{ name: 'secure.product' }">Novo </router-link>
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr v-for="(item, index) in list" :key="index">
+      <td>{{item.name}}</td>
+      <td>{{item.key}}</td>
+      <td>
+           <button type="button" @click="remove($event, item.key)">Delete </button>
+      </td>
+    </tr>
+  </tbody>
+</table>
   `
 });
